d8c5cff93917ded185e7e51ebc3043055d9a0189
m-housni <mohamedhousni1000k@gmail.com>
2025-01-13 17:35:00 +0100

    refactor: enhance data loading functions and improve error handling in dataloader

--- src/utils/dataloader.js
+++ src/utils/dataloader.js
@@ -1,79 +1,93 @@
-const bcrypt = require('bcrypt');
-const config = require('better-config');
+// Import necessary modules
+const bcrypt = require('bcrypt'); // For hashing passwords
+const config = require('better-config'); // For loading configuration files
 
+// Load the configuration file
 config.set(`../../${process.env.CRASH_COURSE_CONFIG_FILE || 'config.json'}`);
 
+// Import Redis client module
 const redis = require('./redisclient');
 
+// Get a Redis client instance
 const redisClient = redis.getClient();
 
+// Name of the consumer group for Redis Streams
 const CONSUMER_GROUP_NAME = 'checkinConsumers';
 
+// Display usage instructions
 const usage = () => {
   console.error('Usage: npm run load users|locations|locationdetails|checkins|indexes|bloom|all');
-  process.exit(0);
+  process.exit(0); // Exit the process with status 0
 };
 
+// Generic function to load data into Redis
 const loadData = async (jsonArray, keyName) => {
-  const pipeline = redisClient.pipeline();
+  const pipeline = redisClient.pipeline(); // Use a pipeline for bulk operations
 
+  // Iterate through the array of JSON objects
   for (const obj of jsonArray) {
+    // Add each object to the Redis pipeline as a hash
     pipeline.hset(redis.getKeyName(keyName, obj.id), obj);
   }
 
+  // Execute the pipeline and handle responses
   const responses = await pipeline.exec();
   let errorCount = 0;
 
+  // Check for errors in the responses
   for (const response of responses) {
     if (response[0] !== null) {
-      errorCount += 1;
+      errorCount += 1; // Increment error count if any operation failed
     }
   }
 
-  return errorCount;
+  return errorCount; // Return the total error count
 };
 
+// Load user data into Redis
 const loadUsers = async () => {
   console.log('Loading user data...');
-  /* eslint-disable global-require */
-  const usersJSON = require('../../data/users.json');
-  /* eslint-enable */
+  const usersJSON = require('../../data/users.json'); // Load users from JSON file
 
-  // Hash the passwords...
-  /* eslint-disable array-callback-return, no-param-reassign */
+  // Hash user passwords before storing
   usersJSON.users.map((user) => {
-    user.password = bcrypt.hashSync(user.password, 5);
+    user.password = bcrypt.hashSync(user.password, 5); // Use bcrypt for password hashing
   });
-  /* eslint-enable */
 
+  // Load user data into Redis and log the result
   const errorCount = await loadData(usersJSON.users, 'users');
   console.log(`User data loaded with ${errorCount} errors.`);
 };
 
+// Load location data into Redis
 const loadLocations = async () => {
   console.log('Loading location data...');
-  /* eslint-disable global-require */
-  const locationsJSON = require('../../data/locations.json');
-  /* eslint-enable */
+  const locationsJSON = require('../../data/locations.json'); // Load locations from JSON file
 
+  // Load location data into Redis and log the result
   const errorCount = await loadData(locationsJSON.locations, 'locations');
   console.log(`Location data loaded with ${errorCount} errors.`);
 };
 
+// Load location details into Redis as JSON
 const loadLocationDetails = async () => {
   console.log('Loading location details...');
-  /* eslint-disable global-require */
-  const locationsJSON = require('../../data/locationdetails.json');
-  /* eslint-enable */
+  const locationsJSON = require('../../data/locationdetails.json'); // Load details from JSON file
 
   const pipeline = redisClient.pipeline();
 
+  // Add each location detail to the pipeline as JSON
   for (const locationDetail of locationsJSON.locationDetails) {
-    pipeline.call('JSON.SET', redis.getKeyName('locationdetails', locationDetail.id), '.', JSON.stringify(locationDetail));
+    pipeline.call(
+      'JSON.SET',
+      redis.getKeyName('locationdetails', locationDetail.id),
+      '.',
+      JSON.stringify(locationDetail)
+    );
   }
 
+  // Execute the pipeline and handle errors
   const responses = await pipeline.exec();
-
   let errorCount = 0;
 
   for (const response of responses) {
@@ -85,56 +99,60 @@ const loadLocationDetails = async () => {
   console.log(`Location detail data loaded with ${errorCount} errors.`);
 };
 
+// Load check-in data as a Redis Stream
 const loadCheckins = async () => {
   console.log('Loading checkin stream entries...');
-
-  /* eslint-disable global-require */
-  const { checkins } = require('../../data/checkins.json');
-  /* eslint-enable */
+  const { checkins } = require('../../data/checkins.json'); // Load check-ins from JSON file
 
   const streamKeyName = redis.getKeyName('checkins');
 
-  // Delete any previous stream.
+  // Delete any previous stream data
   await redisClient.del(streamKeyName);
 
-  // Batch load entries 100 at a time.
+  // Batch load check-ins into Redis Stream
   let n = 0;
   let pipeline = redisClient.pipeline();
 
-  /* eslint-disable no-await-in-loop */
   do {
     const checkin = checkins[n];
-    pipeline.xadd(streamKeyName, checkin.id, 'locationId', checkin.locationId, 'userId', checkin.userId, 'starRating', checkin.starRating);
+    pipeline.xadd(
+      streamKeyName,
+      checkin.id,
+      'locationId',
+      checkin.locationId,
+      'userId',
+      checkin.userId,
+      'starRating',
+      checkin.starRating
+    );
     n += 1;
 
+    // Execute pipeline every 100 entries
     if (n % 100 === 0) {
-      // Send 100 XADD commands to Redis.
       await pipeline.exec();
-
-      // Start a fresh pipeline.
-      pipeline = redisClient.pipeline();
+      pipeline = redisClient.pipeline(); // Start a new pipeline
     }
   } while (n < checkins.length);
-  /* eslint-enable */
 
-  // Send any remaining checkins if the number of checkins in the
-  // file wasn't divisible by 100.
+  // Execute remaining commands in the pipeline
   if (pipeline.length > 0) {
     await pipeline.exec();
   }
 
+  // Log the total number of entries loaded
   const numEntries = await redisClient.xlen(streamKeyName);
   console.log(`Loaded ${numEntries} checkin stream entries.`);
 
+  // Create a consumer group for the stream
   console.log('Creating consumer group...');
   pipeline = redisClient.pipeline();
-
-  pipeline.xgroup('DESTROY', streamKeyName, CONSUMER_GROUP_NAME);
-  pipeline.xgroup('CREATE', streamKeyName, CONSUMER_GROUP_NAME, 0);
+  pipeline.xgroup('DESTROY', streamKeyName, CONSUMER_GROUP_NAME); // Delete existing group if any
+  pipeline.xgroup('CREATE', streamKeyName, CONSUMER_GROUP_NAME, 0); // Create new group
   await pipeline.exec();
   console.log('Consumer group created.');
 };
 
+// Create indexes for searching users and locations
 const createIndexes = async () => {
   console.log('Dropping any existing indexes, creating new indexes...');
 
@@ -142,10 +160,60 @@ const createIndexes = async () => {
   const locationsIndexKey = redis.getKeyName('locationsidx');
 
   const pipeline = redisClient.pipeline();
+
+  // Drop and recreate indexes with specific schemas
   pipeline.call('FT.DROPINDEX', usersIndexKey);
   pipeline.call('FT.DROPINDEX', locationsIndexKey);
-  pipeline.call('FT.CREATE', usersIndexKey, 'ON', 'HASH', 'PREFIX', '1', redis.getKeyName('users'), 'SCHEMA', 'email', 'TAG', 'numCheckins', 'NUMERIC', 'SORTABLE', 'lastSeenAt', 'NUMERIC', 'SORTABLE', 'lastCheckin', 'NUMERIC', 'SORTABLE', 'firstName', 'TEXT', 'lastName', 'TEXT');
-  pipeline.call('FT.CREATE', locationsIndexKey, 'ON', 'HASH', 'PREFIX', '1', redis.getKeyName('locations'), 'SCHEMA', 'category', 'TAG', 'SORTABLE', 'location', 'GEO', 'SORTABLE', 'numCheckins', 'NUMERIC', 'SORTABLE', 'numStars', 'NUMERIC', 'SORTABLE', 'averageStars', 'NUMERIC', 'SORTABLE');
+  pipeline.call(
+    'FT.CREATE',
+    usersIndexKey,
+    'ON',
+    'HASH',
+    'PREFIX',
+    '1',
+    redis.getKeyName('users'),
+    'SCHEMA',
+    'email',
+    'TAG',
+    'numCheckins',
+    'NUMERIC',
+    'SORTABLE',
+    'lastSeenAt',
+    'NUMERIC',
+    'SORTABLE',
+    'lastCheckin',
+    'NUMERIC',
+    'SORTABLE',
+    'firstName',
+    'TEXT',
+    'lastName',
+    'TEXT'
+  );
+  pipeline.call(
+    'FT.CREATE',
+    locationsIndexKey,
+    'ON',
+    'HASH',
+    'PREFIX',
+    '1',
+    redis.getKeyName('locations'),
+    'SCHEMA',
+    'category',
+    'TAG',
+    'SORTABLE',
+    'location',
+    'GEO',
+    'SORTABLE',
+    'numCheckins',
+    'NUMERIC',
+    'SORTABLE',
+    'numStars',
+    'NUMERIC',
+    'SORTABLE',
+    'averageStars',
+    'NUMERIC',
+    'SORTABLE'
+  );
 
   const responses = await pipeline.exec();
 
@@ -157,14 +225,15 @@ const createIndexes = async () => {
   }
 };
 
+// Create a Bloom filter for check-ins
 const createBloomFilter = async () => {
   console.log('Deleting any previous bloom filter, creating new bloom filter...');
 
   const bloomFilterKey = redis.getKeyName('checkinfilter');
 
   const pipeline = redisClient.pipeline();
-  pipeline.del(bloomFilterKey);
-  pipeline.call('BF.RESERVE', bloomFilterKey, 0.0001, 1000000);
+  pipeline.del(bloomFilterKey); // Delete existing Bloom filter
+  pipeline.call('BF.RESERVE', bloomFilterKey, 0.0001, 1000000); // Create a new Bloom filter
 
   const responses = await pipeline.exec();
 
@@ -176,13 +245,15 @@ const createBloomFilter = async () => {
   }
 };
 
+// Main function to determine which data to load
 const runDataLoader = async (params) => {
   if (params.length !== 4) {
-    usage();
+    usage(); // Show usage instructions if arguments are incorrect
   }
 
-  const command = params[3];
+  const command = params[3]; // Get the command argument
 
+  // Determine which action to perform based on the command
   switch (command) {
     case 'users':
       await loadUsers();
@@ -214,7 +285,8 @@ const runDataLoader = async (params) => {
       usage();
   }
 
-  redisClient.quit();
+  redisClient.quit(); // Close Redis connection
 };
 
+// Run the data loader with the provided arguments
 runDataLoader(process.argv);
